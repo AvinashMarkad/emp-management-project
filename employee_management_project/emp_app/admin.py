1ca4f342bd1339427611feb45249be0b67eb6307
AvinashMarkad <markadavi3719@gmail.com>
2024-02-04 20:40:17 +0530

    Fourth commit

--- employee_management_project/emp_app/admin.py
+++ employee_management_project/emp_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Department, Role, Employee
-
+# from .models import Department, Role, Employee
+from .models import Employee
 # Register your models here.
-admin.site.register(Department)
-admin.site.register(Role)
+# admin.site.register(Department)
+# admin.site.register(Role)
 admin.site.register(Employee)
--- employee_management_project/emp_app/views.py
+++ employee_management_project/emp_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Employee
+from .models import Employee 
+import datetime
+
 
 # Create your views here.
 def index (request):
@@ -14,7 +16,22 @@ def view (request):
     return render(request,'view.html',context)
 
 def add (request):
+    if request.method == 'POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        dept=request.POST['dept']
+        salary=request.POST['salary']
+        bonus=request.POST['bonus']
+        role=request.POST['role']
+        phone=request.POST['phone']
+        hire_date=datetime.datetime.now()
+        ins=Employee(first_name=first_name,last_name=last_name,dept=dept,salary=salary,bonus=bonus,role=role,phone=phone,hire_date=hire_date)
+        ins.save()
+        return render(request,'add.html',context = {'success' : True})
     return render(request,'add.html')
+        
+    # elif request.method=='GET':
+    #     return render(request,'add.html')
 
 def remove (request):
     return render(request,'remove.html')
